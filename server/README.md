ffefc6f4c202c082814fcfb36c7ed1bec0405c8e
Connor Musick <cjmusick48@gmail.com>
2020-06-06 09:04:35 -0700

    client and server set up initially

---
+++ server/README.md
@@ -0,0 +1,4 @@
+# connormusick
+A personal website that will host all my app ideas, personal and public.
+
+Built with NodeJS in Express framework and whatever else I decide to add later.
---
+++ server/app.js
@@ -0,0 +1,21 @@
+/*
+*   Main file for the project, most middleware will flow through here before
+*   reaching any route
+*/
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+
+// ROUTES
+const connormusickRoutes = require('./routes/connormusick');
+app.use('/', connormusickRoutes);
+
+app.listen(3000,()=>{
+    console.log('listening on port 3000');
+});
