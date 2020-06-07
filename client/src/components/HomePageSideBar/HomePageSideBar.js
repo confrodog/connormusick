6056516b0cf01799be8834fcf9abf5da3a40b12e
Connor Musick <cjmusick48@gmail.com>
2020-06-07 09:10:26 -0700

    some real structure output

---
+++ client/src/components/HomePageSideBar/HomePageSideBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./HomePageSideBar.css";
+
+class HomePageSideBar extends Component {
+	render() {
+		return (
+			<div className="Home-Page-Sidebar">
+				<p>Sidebar</p>
+			</div>
+		);
+	}
+}
+
+export default HomePageSideBar;
